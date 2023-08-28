de6e15a00a2d30376bae84895b4cf87d51732abf
Alexander <LipatovAlexander93@yandex.ru>
2023-08-28 14:22:17 +0300

    refactor

--- src/main/java/com/github/mnemalotebya/JsonParser.java
+++ src/main/java/com/github/mnemalotebya/JsonParser.java
@@ -59,6 +59,4 @@ public class JsonParser {
             throw new RuntimeException(e);
         }
     }
-
-
 }
--- src/main/java/com/github/mnemalotebya/Main.java
+++ src/main/java/com/github/mnemalotebya/Main.java
@@ -1,11 +1,11 @@
 package com.github.mnemalotebya;
 
 import com.github.mnemalotebya.pojo.Ticket;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static Calendar calendar = Calendar.getInstance();
 
     public static void main(String[] args) {
         List<Ticket> tickets = JsonParser.getTickets();
@@ -19,13 +19,14 @@ public class Main {
                 .filter(ticket -> (ticket.getOrigin().equals("VVO") && ticket.getDestination().equals("TLV")))
                 .collect(Collectors.toMap(
                         Ticket::getCarrier,
-                        t -> (t.getArrivalDate().getTime() - t.getDepartureDate().getTime()),
+                        t -> (t.getDepartureDate().toInstant().until(t.getArrivalDate().toInstant(), ChronoUnit.MINUTES)),
                         Long::min,
                         HashMap::new
                 ));
         for (Map.Entry<String, Long> entry : workMap.entrySet()) {
-            calendar.setTimeInMillis(entry.getValue());
-            System.out.println(entry.getKey() + " - " + calendar.get(Calendar.HOUR) + ":" + calendar.get(Calendar.MINUTE));
+            long hours = entry.getValue() / 60;
+            long minutes = entry.getValue() % 60;
+            System.out.println(entry.getKey() + " - " + hours + " часов " + minutes + " минут");
         }
     }
 
@@ -35,24 +36,19 @@ public class Main {
                 .filter(ticket -> (ticket.getOrigin().equals("VVO") && ticket.getDestination().equals("TLV")))
                 .mapToLong(Ticket::getPrice).average().orElse(0);
 
-        var ref = new Object() {
-            int medina;
-        };
-
         List<Ticket> sortedTickets = tickets
                 .stream()
                 .filter(ticket -> (ticket.getOrigin().equals("VVO") && ticket.getDestination().equals("TLV")))
                 .sorted(Comparator.comparing(Ticket::getPrice))
                 .toList();
-        sortedTickets
-                .forEach(ticket -> {
-                    int medinaList = sortedTickets.size() / 2;
-                    if (sortedTickets.size() % 2 == 0) {
-                        ref.medina = Math.toIntExact((sortedTickets.get(medinaList).getPrice()) + (sortedTickets.get(medinaList + 1).getPrice()) /2);
-                    } else {
-                        ref.medina = Math.toIntExact((sortedTickets.get(medinaList + 1).getPrice()));
-                    }
-                });
-        return avg - ref.medina;
+
+        int medina;
+        int medinaList = sortedTickets.size() / 2;
+        if (sortedTickets.size() % 2 == 0) {
+            medina = (sortedTickets.get(medinaList - 1).getPrice().intValue() + sortedTickets.get(medinaList).getPrice().intValue()) / 2;
+        } else {
+            medina = sortedTickets.get(medinaList).getPrice().intValue();
+        }
+        return avg - medina;
     }
 }
